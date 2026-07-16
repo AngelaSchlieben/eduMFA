864505d794a5879685f291eb108fedf8a931e6ef
Angela Schlieben <angela.schlieben@gmail.com>
2026-07-16 16:36:35 +0200

    fix: UI/UX and code improvements for timeframe and stats selection
    - Changed default timeframe from "All" to "1 Day"
    - Refactored timeframe calculation for calculating the start date
    - Removed hardcoded stats keys, now uses getAvailableStatsKeys()
    - Colors are now assigned dynamically; generateRandomColor() added
    - removed most of the formatting
    - Replaced icon-based  legend with html-Elements
    - Removed redundant action labels
    - Function and variable names changed to camelCase

--- edumfa/static/components/dashboard/controllers/dashboardControllers.js
+++ edumfa/static/components/dashboard/controllers/dashboardControllers.js
@@ -35,10 +35,6 @@ myApp.controller("dashboardController", ["ConfigFactory", "TokenFactory",
             "inactive": [], "num_inactive": 0
         };
         $scope.authentications = { "success": 0, "fail": 0 };
-        $scope.tokens_timeline = {
-            datasets: []
-        };
-
 
         $scope.get_total_token_number = function () {
             // We call getTokens with pagesize=0, so that we do
@@ -173,88 +169,103 @@ myApp.controller("dashboardController", ["ConfigFactory", "TokenFactory",
             return 0;
         };
 
-        $scope.colorMap = {
-            hardware_tokens: '#4e79a7',
-            software_tokens: '#f28e2b',
-            total_tokens: '#e15759',
-            user_with_token: '#76b7b2',
-            assigned_tokens: '#59a14f',
-            unassigned_hardware_tokens: '#edc948'
+        $scope.tokenTimeline = {
+            datasets: []
         };
 
-        $scope.available_stats_keys = [
-            "total_tokens",
-            "hardware_tokens",
-            "software_tokens",
-            "unassigned_hardware_tokens",
-            "assigned_tokens",
-            "user_with_token"
-        ];
-
-        $scope.stats_keys_liste = []
-        $scope.available_stats_keys.forEach(function (sk) {
-            $scope.stats_keys_liste.push({ name: sk })
-        });
-
-        $scope.time_frame = [
-            "All",
-            "1 Day",
-            "1 Week",
-            "1 Month",
-            "3 Month",
-            "6 Month",
-            "1 Year"
+        $scope.timeFrame = [
+            { label: "1 Day", unit: "day", amount: 1 },
+            { label: "1 Week", unit: "day", amount: 7 },
+            { label: "1 Month", unit: "month", amount: 1 },
+            { label: "3 Months", unit: "month", amount: 3 },
+            { label: "6 Months", unit: "month", amount: 6 },
+            { label: "1 Year", unit: "year", amount: 1 },
+            { label: "All", unit: "year", amount: 100 }
         ]
 
-        $scope.selected_start_time = $scope.time_frame[0];
+        $scope.selectedTimeFrame = $scope.timeFrame[0];
+        $scope.selectedStatsKeys = [];
+        $scope.availableStatsKeys = [];
+        
 
-        $scope.get_tokens_timeline = function () {
-            $scope.tokens_timeline = {
-                datasets: []
-            };
+        $scope.generateRandomColor = function () {
+            const chars = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += chars[Math.floor(Math.random() * 16)];
+            }
+            return color
+        }
 
+        $scope.getAvailableStatsKeys = function () {
+            $scope.selectedStatsKeys = [];
+            $scope.availableStatsKeys = [];
+            MonitoringFactory.get_stats_keys(function (data) {
+                var newList = [];
+                data.result.value.forEach(function (key) {
+                    newList.push({
+                        id: key,
+                        name: key,
+                        color: $scope.generateRandomColor(),
+                        selected: false
+                    });
+                });
+                $scope.availableStatsKeys = newList;
+            });
+        };
+
+        $scope.getStartTime = function (selectedTimeFrame) {
             var today = new Date()
-            var x = $scope.selected_start_time[0]
             var day = today.getDate()
             var month = today.getMonth()
             var year = today.getFullYear()
 
-
-            if ($scope.selected_start_time === "All") {
-                $scope.start_time = ""
+            if (selectedTimeFrame.unit === "day") {
+                day = day - selectedTimeFrame.amount
             }
-            else {
-                if ($scope.selected_start_time.endsWith("Day")) {
-                    day = day - x
-                }
-                else if ($scope.selected_start_time.endsWith("Week")) {
-                    day = day - (x * 7)
-                }
-                else if ($scope.selected_start_time.endsWith("Month")) {
-                    month = month - x
-                }
-                else if ($scope.selected_start_time.endsWith("Year")) {
-                    year = year - x
-                }
-                $scope.start_time = new Date(year, month, day)
+            if (selectedTimeFrame.unit === "month") {
+                month = month - selectedTimeFrame.amount
             }
-            console.log($scope.start_time)
+            if (selectedTimeFrame.unit === "year") {
+                year = year - selectedTimeFrame.amount
+            }
+            return new Date(year, month, day)
+        }
 
-            $scope.selected_stats_keys = $scope.selected_stats_keys_objs.map(o => o.name);
-            $scope.selected_stats_keys.forEach(sk =>
-                MonitoringFactory.get_monitored(sk, {start: $scope.start_time }, function (data) {
+        $scope.changeHidden = function (key) {
+            var ds = $scope.tokenTimeline.datasets.find(d => d.label === key.name);
+            if (ds) {
+                ds.hidden = !ds.hidden;
+            }
+        }
+
+        $scope.clearTimeLine = function () {
+            $scope.tokenTimeline = {
+                datasets: []
+            };
+        }
+
+        $scope.getTokenTimeline = function () {
+            $scope.clearTimeLine()
+            var newTimeline = []
+            var startTime = $scope.getStartTime($scope.selectedTimeFrame)
+
+            $scope.selectedStatsKeys.forEach(sk =>
+                MonitoringFactory.get_monitored(sk.name, { start: startTime }, function (data) {
                     var d = data.result.value;
-                    var color = $scope.colorMap[sk];
+                    var color = sk.color;
                     var dataset = {
-                        label: sk,
+                        label: sk.name,
                         data: d.map(e => ({ x: new Date(e[0].replace(" ", "T")), y: e[1] })),
                         borderColor: color,
                         backgroundColor: color,
                         pointBackgroundColor: color,
                         hidden: false
                     }
-                    $scope.tokens_timeline.datasets.push(dataset)
-                }));
+                    newTimeline.push(dataset)
+                }),
+            )
+            $scope.tokenTimeline.datasets = newTimeline
         };
 
         if (AuthFactory.checkRight('tokenlist')) {
@@ -262,7 +273,6 @@ myApp.controller("dashboardController", ["ConfigFactory", "TokenFactory",
             $scope.get_token_hardware();
             $scope.get_token_software();
         }
-
         if (AuthFactory.checkRight('policyread')) {
             $scope.get_policies();
         }
@@ -272,6 +282,9 @@ myApp.controller("dashboardController", ["ConfigFactory", "TokenFactory",
         if (AuthFactory.checkRight('auditlog')) {
             $scope.getAuthentication();
             $scope.getAdministration();
+        }
+        if (AuthFactory.checkRight('statistics_read')) {
+            $scope.getAvailableStatsKeys();
         }
 
         // listen to the reload broadcast
@@ -292,8 +305,7 @@ myApp.controller("dashboardController", ["ConfigFactory", "TokenFactory",
                 $scope.getAdministration();
             }
             if (AuthFactory.checkRight('statistics_read')) {
-                $scope.get_tokens_timeline();
+                $scope.getAvailableStatsKeys();
             }
-
         });
     }]);
